7d7e167d09d2f829689667382cc9726c9dc127d1
Danish Farooqui <danishfarooqui2018@gmail.com>
2024-07-29 20:32:18 +0530

    React

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
+    <>
     <div>
       <h1 style={{color:'blue',textAlign:'center',fontSize:50}}>My Home Page</h1>
     </div>
+    </>
   )
 }
 
